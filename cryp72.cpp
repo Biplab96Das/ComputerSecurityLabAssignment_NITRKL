47a7bddde1badfdfefccd0f18c4feac75f256415
Biplab Das <31479620+Biplab96Das@users.noreply.github.com>
2022-11-11 15:42:15 +0530

    Add files via upload

---
+++ cryp72.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+int m_in(int a,int b)
+{
+int mod=b,n=a;
+int i,q,r1=mod,r2=n,r,t1=0,t2=1,t;
+    while(r2 != 0)
+    {
+    q=r1/r2;
+    r=r1%r2;
+    t=t1-q*t2;
+
+    r1=r2;
+    r2=r;
+    t1=t2;
+    t2=t;
+    }
+if(t1>0){
+     i=t1 % mod;
+    return i;
+ }
+else{
+    while(t1<0)
+    { int mulF=1;
+      t1=t1 + mulF*mod;
+      mulF++;
+    }
+    i=t1 % mod;
+    return i;
+   }    
+        
+}
+
+int main(){
+	int x,n,md=1;
+	vector <int> a;
+	vector <int> m;
+	vector <int> mr;
+	vector <int> mi;
+	cout<<"Enter number of equations to enter : ";
+	cin>>n;
+
+	for(int i=0;i<n;i++){
+		int a1,m1;
+		cout<<"Enter values of a"<<i+1<<"and m"<<i+1<<" in equation x = a"<<i+1<<" mod m"<<i+1<<" : ";
+		cin>>a1>>m1;
+		a.push_back(a1);
+		m.push_back(m1);
+	}
+	
+
+	for(int i=0;i<m.size();i++){
+		md=md*m[i];
+	}
+
+	for(int i=0;i<m.size();i++){
+		mr.push_back(md/m[i]);
+	}
+
+	for(int i=0;i<m.size();i++){
+		cout<<mr[i]<<" "<<m[i]<<endl; 
+		int k = m_in(mr[i],m[i]);
+		mi.push_back(k);
+
+	}
+
+	for(int i=0;i<m.size();i++){
+		cout<<m[i]<<"\t";
+	}
+	cout<<endl;
+
+
+	for(int i=0;i<m.size();i++){
+		cout<<mr[i]<<"\t";
+	}
+	cout<<endl;
+
+	for(int i=0;i<m.size();i++){
+		cout<<mi[i]<<"\t";
+	}
+	cout<<endl;
+	x=0;
+	for(int i=0;i<m.size();i++){
+		x+=(a[i]*mr[i]*mi[i]);
+	}
+	x%=md;
+	cout<<x<<endl;
+
+
+
+}
